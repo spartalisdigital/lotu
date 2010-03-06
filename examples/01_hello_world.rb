ad21eccd8a7eab6e1e02850bbec219d88780ac58
lotu <dev@lobotuerto.com>
2010-03-06 02:36:02 -0600

    Some metrics and moving code around

--- examples/01_hello_world.rb
+++ examples/01_hello_world.rb
@@ -3,6 +3,15 @@ require File.expand_path(LIB_PATH)
 include Gosu::Button
 
 class Player < Lotu::Actor
+  def initialize(parent)
+    super
+    set_image('Soldier.png')
+    set_keys(KbRight => [:move_right, 0],
+             KbLeft => [:move_left, 0],
+             KbUp => [:move_up, 0],
+             KbDown => [:move_down, 0])
+  end
+
   def move_right
     @x += 1
   end
@@ -25,14 +34,8 @@ class Example < Lotu::Window
     super
     set_keys(KbEscape => :close)
     load_resources('media')
-    
     @player = Player.new(self)
-    @player.set_keys(KbRight => [:move_right, 0],
-                     KbLeft => [:move_left, 0],
-                     KbUp => [:move_up, 0],
-                     KbDown => [:move_down, 0])
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
-    @player.set_image('Soldier.png')
   end
 
   def draw
@@ -40,5 +43,5 @@ class Example < Lotu::Window
     @font.draw("FPS: #{@fps}", 10, 10, 0, 1.0, 1.0, 0xffffff00)
   end
 end
-puts Lotu.game_path
+
 Example.new.show
--- lib/lotu/fps.rb
+++ lib/lotu/fps.rb
@@ -6,6 +6,8 @@ class FpsCounter
     @ticks = 0
     @fps = 0.0
     @samples = samples
+
+    @objs = @actors = @input_controllers = @lotu = 0
   end
 
   def update(elapsed_t)
@@ -15,10 +17,13 @@ class FpsCounter
       @fps = 1000/(@accum/@ticks)
       @ticks = 0
       @accum = 0.0
+      @objs = ObjectSpace.each_object.count
+      @actors = ObjectSpace.each_object(Lotu::Actor).count
+      @inputs = ObjectSpace.each_object(Lotu::InputController).count
     end
   end
 
   def to_s
-    "Samples: #{@samples} FPS: #{format("%.2f",@fps)}"
+    "Samples: #{@samples} FPS: #{format("%.2f",@fps)} Objs: #{@objs} Acts: #{@actors} InputsCs: #{@inputs}"
   end
 end
