248ed10238668e166ea2057fea08f8c5161a1923
lotu <dev@lobotuerto.com>
2010-03-06 01:08:00 -0600

    New folder structure

--- lib/lotu.rb
+++ lib/lotu.rb
@@ -3,11 +3,12 @@ LOTU_ROOT = File.expand_path(File.join(File.dirname(__FILE__), 'lotu'))
 $LOAD_PATH.unshift(LOTU_ROOT)
 
 require 'gosu'
-require 'controllable'
 
+require 'behaviors/controllable'
 require 'behaviors/resourceful'
 require 'behaviors/drawable'
-require 'input_controller'
+require 'behaviors/input_controller'
+
 require 'actor'
 require 'window'
 
