f27d99fb178c6d667fa6ce3cbe7db3b036008cff
lotu <dev@lobotuerto.com>
2010-03-27 03:12:51 -0600

    Little changes on file load reporting

--- lib/lotu/game.rb
+++ lib/lotu/game.rb
@@ -7,12 +7,12 @@ module Lotu
 
     include SystemUser
 
-    def initialize(params={})
+    def initialize(opts={})
       super(800, 600, false)
 
       # Handy global window variable
       $lotu = self
-      @debug = false
+      @debug = opts[:debug] || false
       setup_containers
 
       # For timer initialization
@@ -140,24 +140,34 @@ module Lotu
     end
 
     def load_images(path)
+      count = 0
       with_files(/\.png|\.jpg|\.bmp/, path) do |file_name, file_path|
+        count += 1
         @images[file_name] = Gosu::Image.new($lotu, file_path)
       end
+      puts "\n#{count} image(s) loaded."
     end
 
     def load_sounds(path)
+      count = 0
       with_files(/\.ogg|\.mp3|\.wav/, path) do |file_name, file_path|
+        count += 1
         @sounds[file_name] = Gosu::Sample.new($lotu, file_path)
       end
+      puts "\n#{count} sounds(s) loaded."
     end
 
     def load_songs(path)
+      count = 0
       with_files(/\.ogg|\.mp3|\.wav/, path) do |file_name, file_path|
+        count += 1
         @songs[file_name] = Gosu::Song.new($lotu, file_path)
       end
+      puts "\n#{count} song(s) loaded."
     end
 
     def load_animations(path)
+      count = 0
       coords = Hash.new{|h,k| h[k] = []}
 
       with_files(/\.txt/, path) do |file_name, file_path|
@@ -167,14 +177,17 @@ module Lotu
             coords[name] << line.scan(/\d+/).map!(&:to_i)
           end
         end
+        false
       end
 
       with_files(/\.png|\.jpg|\.bmp/, path) do |file_name, file_path|
         name, extension = file_name.split('.')
+        count += 1 if coords[name]
         coords[name].each do |index, x, y, width, height|
           @animations[file_name] << Gosu::Image.new($lotu, file_path, true, x, y, width, height)
         end
       end
+      puts "\n#{count} animation(s) loaded."
     end
 
     def with_path_from_file(path, &blk)
@@ -184,20 +197,17 @@ module Lotu
 
     def with_files(regexp, path)
       path = File.expand_path(File.join(@path, path))
-      puts "\nLoading from: #{path}"
+      puts "\nLoading from: #{path}".blue if @debug
 
-      count = 0
       Dir.entries(path).grep(regexp).each do |entry|
         begin
-          yield(entry, File.join(path, entry))
-          count += 1
-          print '.'.green
+          report = yield(entry, File.join(path, entry))
+          print '.'.green if report
         rescue Exception => e
           print '.'.red
           puts e, File.join(path, entry) if @debug
         end
       end
-      puts "\n#{count} file(s) loaded."
     end
 
   end
